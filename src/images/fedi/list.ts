ca7139c28b89e7cc5165415501b12e28014abf5d
thetalemon <sasami0314@gmail.com>
2024-02-25 21:17:24 +0900

    fediを追加しつつ整理

---
+++ src/images/fedi/list.ts
@@ -0,0 +1,28 @@
+import Image202301 from './202301.png'
+import Image202302 from './202302.png'
+import Image202303 from './202303.png'
+import Image202304 from './202304.png'
+import Image202305 from './202305.png'
+
+export const fediverseList = [
+  {
+    image: Image202305,
+    alt: 'にゃんぷぷつめあわせっと',
+  },
+  {
+    image: Image202304,
+    alt: 'はーとのにゃんぷぷ',
+  },
+  {
+    image: Image202303,
+    alt: 'なかよしにゃんぷっぷーとうさぷっぷー',
+  },
+  {
+    image: Image202302,
+    alt: 'にゃんぷっぷーとうさぷっぷー',
+  },
+  {
+    image: Image202301,
+    alt: 'あざらしとにゃんぷっぷーの邂逅',
+  },
+]
--- src/images/other/list.ts
+++ src/images/other/list.ts
@@ -2,22 +2,27 @@ import Image202201 from './202201.png'
 import Image202202 from './202202.png'
 import Image202203 from './202203.png'
 import Image202204 from './202204.png'
+import Image202301 from './202301.png'
 
 export const otherList = [
   {
-    image: Image202201,
-    alt: '紫の髪のウインクした子',
+    image: Image202301,
+    alt: 'Fediverse来た時の自己紹介イラスト',
   },
   {
-    image: Image202202,
-    alt: 'ピンクの髪の花飾りの子',
+    image: Image202204,
+    alt: 'インナーカラーオレンジのメッセンジャーバッグを持った子',
   },
   {
     image: Image202203,
     alt: '緑の髪の制服の子',
   },
   {
-    image: Image202204,
-    alt: 'インナーカラーオレンジのメッセンジャーバッグを持った子',
+    image: Image202202,
+    alt: 'ピンクの髪の花飾りの子',
+  },
+  {
+    image: Image202201,
+    alt: '紫の髪のウインクした子',
   },
 ]
