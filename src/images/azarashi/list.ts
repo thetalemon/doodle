d57b707b9bb576f4dfbfae8b1cfbc321682aa366
thetalemon <sasami0314@gmail.com>
2024-02-26 00:14:28 +0900

    残りのあざらし画像をアップロード

--- src/images/azarashi/list.ts
+++ src/images/azarashi/list.ts
@@ -1,5 +1,22 @@
 import Image202401 from './202401.png'
 import Image202402 from './202402.png'
+import Image202301 from './202301.png'
+import Image202302 from './202302.png'
+import Image202303 from './202303.jpg'
+import Image202304 from './202304.png'
+import Image202305 from './202305.png'
+import Image202306 from './202306.png'
+import Image202307 from './202307.png'
+import Image202308 from './202308.png'
+import Image202309 from './202309.png'
+import Image202310 from './202310.png'
+import Image202311 from './202311.png'
+import Image202312 from './202312.png'
+import Image202313 from './202313.png'
+import Image202314 from './202314.png'
+import Image202315 from './202315.png'
+import Image202316 from './202316.png'
+import Image202317 from './202317.png'
 
 export const azarashiList = [
   {
@@ -9,5 +26,73 @@ export const azarashiList = [
   {
     image: Image202402,
     alt: '紫のブランケットにくるまるあざらし',
+  },
+  {
+    image: Image202301,
+    alt: '鯖缶のあざらし',
+  },
+  {
+    image: Image202302,
+    alt: 'シンプルな青いあざらし',
+  },
+  {
+    image: Image202303,
+    alt: 'アホそうなあざらし',
+  },
+  {
+    image: Image202304,
+    alt: 'シンプルな青いあざらし なまえつき',
+  },
+  {
+    image: Image202305,
+    alt: 'シンプルなピンクのあざらし なまえつき',
+  },
+  {
+    image: Image202306,
+    alt: 'ピンクのあざらし 天使のはねつき',
+  },
+  {
+    image: Image202307,
+    alt: 'おひさしぶりのあざらし',
+  },
+  {
+    image: Image202308,
+    alt: 'パリピ孔明のコスプレをするあざらし',
+  },
+  {
+    image: Image202309,
+    alt: 'アンデッドアンラックの風子のコスプレをするあざらし',
+  },
+  {
+    image: Image202310,
+    alt: '注射刺されそうなあざらし',
+  },
+  {
+    image: Image202311,
+    alt: '注射されたあざらし',
+  },
+  {
+    image: Image202312,
+    alt: 'しんぷるあざらし',
+  },
+  {
+    image: Image202313,
+    alt: 'フリーレンの仮装をするあざらし',
+  },
+  {
+    image: Image202314,
+    alt: '読書あざらし',
+  },
+  {
+    image: Image202315,
+    alt: 'メンダコになりたいあざらし',
+  },
+  {
+    image: Image202316,
+    alt: 'ちっちゃいあざらしつめあわせっと',
+  },
+  {
+    image: Image202317,
+    alt: '献血中のあざらし',
   },
 ]
