8993d9823935f8d2fffdc18a31d7416430948c48
thetalemon <sasami0314@gmail.com>
2024-02-26 22:35:53 +0900

    npm run format

--- src/images/azarashi/list.ts
+++ src/images/azarashi/list.ts
@@ -1,98 +1,98 @@
-import Image202401 from './2024/1.png'
-import Image202402 from './2024/2.png'
-import Image202301 from './2023/1.png'
-import Image202302 from './2023/2.png'
-import Image202303 from './2023/3.png'
-import Image202304 from './2023/4.png'
-import Image202305 from './2023/5.png'
-import Image202306 from './2023/6.png'
-import Image202307 from './2023/7.png'
-import Image202308 from './2023/8.png'
-import Image202309 from './2023/9.png'
-import Image202310 from './2023/10.png'
-import Image202311 from './2023/11.png'
-import Image202312 from './2023/12.png'
-import Image202313 from './2023/13.png'
-import Image202314 from './2023/14.png'
-import Image202315 from './2023/15.jpg'
-import Image202316 from './2023/16.png'
-import Image202317 from './2023/17.png'
+import Image202401 from "./2024/1.png";
+import Image202402 from "./2024/2.png";
+import Image202301 from "./2023/1.png";
+import Image202302 from "./2023/2.png";
+import Image202303 from "./2023/3.png";
+import Image202304 from "./2023/4.png";
+import Image202305 from "./2023/5.png";
+import Image202306 from "./2023/6.png";
+import Image202307 from "./2023/7.png";
+import Image202308 from "./2023/8.png";
+import Image202309 from "./2023/9.png";
+import Image202310 from "./2023/10.png";
+import Image202311 from "./2023/11.png";
+import Image202312 from "./2023/12.png";
+import Image202313 from "./2023/13.png";
+import Image202314 from "./2023/14.png";
+import Image202315 from "./2023/15.jpg";
+import Image202316 from "./2023/16.png";
+import Image202317 from "./2023/17.png";
 
 export const azarashiList = [
   {
     image: Image202402,
-    alt: '紫のブランケットにくるまるあざらし',
+    alt: "紫のブランケットにくるまるあざらし",
   },
   {
     image: Image202401,
-    alt: 'みかんを頭にのせたあざらし',
+    alt: "みかんを頭にのせたあざらし",
   },
   {
     image: Image202317,
-    alt: '鯖缶のあざらし',
+    alt: "鯖缶のあざらし",
   },
   {
     image: Image202316,
-    alt: 'シンプルな青いあざらし',
+    alt: "シンプルな青いあざらし",
   },
   {
     image: Image202315,
-    alt: 'アホそうなあざらし',
+    alt: "アホそうなあざらし",
   },
   {
     image: Image202314,
-    alt: 'シンプルな青いあざらし なまえつき',
+    alt: "シンプルな青いあざらし なまえつき",
   },
   {
     image: Image202313,
-    alt: 'シンプルなピンクのあざらし なまえつき',
+    alt: "シンプルなピンクのあざらし なまえつき",
   },
   {
     image: Image202312,
-    alt: 'ピンクのあざらし 天使のはねつき',
+    alt: "ピンクのあざらし 天使のはねつき",
   },
   {
     image: Image202311,
-    alt: 'おひさしぶりのあざらし',
+    alt: "おひさしぶりのあざらし",
   },
   {
     image: Image202310,
-    alt: 'パリピ孔明のコスプレをするあざらし',
+    alt: "パリピ孔明のコスプレをするあざらし",
   },
   {
     image: Image202309,
-    alt: 'アンデッドアンラックの風子のコスプレをするあざらし',
+    alt: "アンデッドアンラックの風子のコスプレをするあざらし",
   },
   {
     image: Image202308,
-    alt: '注射刺されそうなあざらし',
+    alt: "注射刺されそうなあざらし",
   },
   {
     image: Image202307,
-    alt: '注射されたあざらし',
+    alt: "注射されたあざらし",
   },
   {
     image: Image202306,
-    alt: 'しんぷるあざらし',
+    alt: "しんぷるあざらし",
   },
   {
     image: Image202305,
-    alt: 'フリーレンの仮装をするあざらし',
+    alt: "フリーレンの仮装をするあざらし",
   },
   {
     image: Image202304,
-    alt: '読書あざらし',
+    alt: "読書あざらし",
   },
   {
     image: Image202303,
-    alt: 'メンダコになりたいあざらし',
+    alt: "メンダコになりたいあざらし",
   },
   {
     image: Image202302,
-    alt: 'ちっちゃいあざらしつめあわせっと',
+    alt: "ちっちゃいあざらしつめあわせっと",
   },
   {
     image: Image202301,
-    alt: '献血中のあざらし',
+    alt: "献血中のあざらし",
   },
-]
+];
--- src/images/other/list.ts
+++ src/images/other/list.ts
@@ -1,28 +1,28 @@
-import Image202201 from './2022/1.png'
-import Image202202 from './2022/2.png'
-import Image202203 from './2022/3.png'
-import Image202204 from './2022/4.png'
-import Image202301 from './2023/1.png'
+import Image202201 from "./2022/1.png";
+import Image202202 from "./2022/2.png";
+import Image202203 from "./2022/3.png";
+import Image202204 from "./2022/4.png";
+import Image202301 from "./2023/1.png";
 
 export const otherList = [
   {
     image: Image202301,
-    alt: 'Fediverse来た時の自己紹介イラスト',
+    alt: "Fediverse来た時の自己紹介イラスト",
   },
   {
     image: Image202201,
-    alt: 'インナーカラーオレンジのメッセンジャーバッグを持った子',
+    alt: "インナーカラーオレンジのメッセンジャーバッグを持った子",
   },
   {
     image: Image202202,
-    alt: '緑の髪の制服の子',
+    alt: "緑の髪の制服の子",
   },
   {
     image: Image202203,
-    alt: 'ピンクの髪の花飾りの子',
+    alt: "ピンクの髪の花飾りの子",
   },
   {
     image: Image202204,
-    alt: '紫の髪のウインクした子',
+    alt: "紫の髪のウインクした子",
   },
-]
+];
