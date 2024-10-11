ca513fc75e67ec21542cfb803854b913d646bd15
thetalemon <sasami0314@gmail.com>
2024-10-11 23:05:04 +0900

    sitemap追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://doodle.manasas.dev/',
+  integrations: [sitemap()],
   vite: {
     css: {
       preprocessorOptions: {
