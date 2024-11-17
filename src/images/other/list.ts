87cc378994b82bd36a0218d51752ce44d105b80a
thetalemon <sasami0314@gmail.com>
2024-11-17 20:12:58 +0900

    4点追加 + 更新履歴とnewアイコンを追加

--- src/images/other/list.ts
+++ src/images/other/list.ts
@@ -3,6 +3,11 @@ import Image202202 from './2022/2.png'
 import Image202203 from './2022/3.png'
 import Image202204 from './2022/4.png'
 import Image202301 from './2023/1.png'
+import ImageOld01 from './old/1.png'
+import ImageOld02 from './old/2.png'
+import ImageOld03 from './old/3.png'
+import ImageOld04 from './old/4.png'
+import ImageOld05 from './old/5.png'
 
 export const otherList = [
   {
@@ -24,5 +29,30 @@ export const otherList = [
   {
     image: Image202204,
     alt: '紫の髪のウインクした子',
+  },
+  {
+    image: ImageOld01,
+    alt: 'ふわっとした雰囲気の茶髪の子',
+    new: true,
+  },
+  {
+    image: ImageOld02,
+    alt: '提灯もった和風の狐面の子',
+    new: true,
+  },
+  {
+    image: ImageOld03,
+    alt: '緑の制服っぽい子',
+    new: true,
+  },
+  {
+    image: ImageOld04,
+    alt: 'どんぐり持った緑の妖精',
+    new: true,
+  },
+  {
+    image: ImageOld05,
+    alt: 'オレンジの髪の毛の子',
+    new: true,
   },
 ]
--- src/type/images.d.ts
+++ src/type/images.d.ts
@@ -1,4 +1,5 @@
 export type ImageListItem = {
   image: ImageMetadata
   alt: string
+  new?: boolean
 }
