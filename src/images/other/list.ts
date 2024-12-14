27528d0482454c6a262670c60dd6f2351df99917
thetalemon <sasami0314@gmail.com>
2024-12-14 18:13:52 +0900

    4点追加

--- src/images/other/list.ts
+++ src/images/other/list.ts
@@ -1,6 +1,8 @@
 import Image202401 from './2024/1.png'
 import Image202402 from './2024/2.jpg'
 import Image202403 from './2024/3.png'
+import Image202404 from './2024/4.png'
+import Image202405 from './2024/5.png'
 import Image202301 from './2023/1.png'
 import Image202201 from './2022/1.png'
 import Image202202 from './2022/2.png'
@@ -11,21 +13,30 @@ import ImageOld02 from './old/2.png'
 import ImageOld03 from './old/3.png'
 import ImageOld04 from './old/4.png'
 import ImageOld05 from './old/5.png'
+import ImageOld06 from './old/6.png'
+import ImageOld07 from './old/7.png'
 
 export const otherList = [
   {
     image: Image202401,
     alt: 'ふわっとした青い子の絵',
-    new: true,
   },
   {
     image: Image202402,
     alt: '赤くて吸血鬼っぽい絵',
-    new: true,
   },
   {
     image: Image202403,
     alt: '金髪の子の青い絵',
+  },
+  {
+    image: Image202404,
+    alt: '褐色肌いいよね',
+    new: true,
+  },
+  {
+    image: Image202405,
+    alt: '黒髪赤服の子',
     new: true,
   },
   {
@@ -51,26 +62,31 @@ export const otherList = [
   {
     image: ImageOld01,
     alt: 'ふわっとした雰囲気の茶髪の子',
-    new: true,
   },
   {
     image: ImageOld02,
     alt: '提灯もった和風の狐面の子',
-    new: true,
   },
   {
     image: ImageOld03,
     alt: '緑の制服っぽい子',
-    new: true,
   },
   {
     image: ImageOld04,
     alt: 'どんぐり持った緑の妖精',
-    new: true,
   },
   {
     image: ImageOld05,
     alt: 'オレンジの髪の毛の子',
+  },
+  {
+    image: ImageOld06,
+    alt: '紫の子',
+    new: true,
+  },
+  {
+    image: ImageOld07,
+    alt: '緑とピンクの子',
     new: true,
   },
 ]
